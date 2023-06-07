edb9ae0469c4daaf475116424129a02cd087b91b
guillaume bt <guillaume.bacot.ey@gmail.com>
2023-06-07 08:40:41 +0000

    staging : details des tables accidents

---
+++ models/staging/stg_accidents/stg_caracteristiques_accidents.sql
@@ -0,0 +1,64 @@
+{{ config(
+    materialized='view',
+)}}
+
+SELECT 
+Num_Acc,
+FORMAT_TIMESTAMP('%Y-%m-%d', TIMESTAMP(DATE(CONCAT(CAST(an AS STRING), '-', CAST(mois AS STRING), '-', CAST(jour AS STRING))))) AS date_date,
+hrmm  as heure_minute,
+    CASE 
+        WHEN lum = 1 THEN 'Plein jour'
+        WHEN lum = 2 THEN 'Crépuscule ou aube'
+        WHEN lum = 3 THEN 'Nuit sans éclairage public'
+        WHEN lum = 4 THEN 'Nuit avec éclairage public non allumé'
+        WHEN lum = 5 THEN 'Nuit avec éclairage public allumé'
+        ELSE ' '
+    END as lumiere,
+dep as departement,
+com as commune,
+    CASE 
+        WHEN agg = 1 THEN 'Hors agglomération'
+        WHEN agg = 2 THEN 'En agglomération'
+        ELSE ' '
+    END as agglomeration,
+    CASE
+        WHEN int = 1 THEN 'Hors intersection'
+        WHEN int = 2 THEN 'Intersection en X'
+        WHEN int = 3 THEN 'Intersection en T'
+        WHEN int = 4 THEN 'Intersection en Y'
+        WHEN int = 5 THEN 'Intersection à plus de 4 branches'
+        WHEN int = 6 THEN 'Giratoire'
+        WHEN int = 7 THEN 'Place'
+        WHEN int = 8 THEN 'Passage à niveau'
+        WHEN int = 9 THEN 'Autre intersection'
+        ELSE ' '
+    END as intersection,
+    CASE
+        WHEN atm = -1 THEN 'Non renseigné'
+        WHEN atm = 1 THEN 'Normale'
+        WHEN atm = 2 THEN 'Pluie légère'
+        WHEN atm = 3 THEN 'Pluie forte'
+        WHEN atm = 4 THEN 'Neige - grêle'
+        WHEN atm = 5 THEN 'Brouillard - fumée'
+        WHEN atm = 6 THEN 'Vent fort - tempête'
+        WHEN atm = 7 THEN 'Temps éblouissant'
+        WHEN atm = 8 THEN 'Temps couvert'
+        WHEN atm = 9 THEN 'Autre'
+        ELSE ' '
+    END as meteo,
+    CASE
+        WHEN col = -1 THEN 'Non renseigné'
+        WHEN col = 1 THEN 'Deux véhicules - frontale'
+        WHEN col = 2 THEN 'Deux véhicules – par l’arrière'
+        WHEN col = 3 THEN 'Deux véhicules – par le coté'
+        WHEN col = 4 THEN 'Trois véhicules et plus – en chaîne'
+        WHEN col = 5 THEN 'Trois véhicules et plus - collisions multiples'
+        WHEN col = 6 THEN 'Autre collision'
+        WHEN col = 7 THEN 'Sans collision'
+        ELSE ' '
+    END as collision,
+COALESCE(adr,'') as adresse,
+COALESCE(lat, '') as latitude,
+COALESCE(long, '') as longitude
+
+FROM velyon-batch-1187.accident.caracteristiques_all
---
+++ models/staging/stg_accidents/stg_lieux_accidents.sql
@@ -0,0 +1,100 @@
+{{ config(
+    materialized='view',
+)}}
+
+SELECT 
+Num_Acc,
+    CASE 
+        WHEN catr = 1 THEN 'Autoroute'
+        WHEN catr = 2 THEN 'Route nationale'
+        WHEN catr = 3 THEN 'Route Départementale'
+        WHEN catr = 4 THEN 'Voie Communales'
+        WHEN catr = 5 THEN 'Hors réseau public'
+        WHEN catr = 6 THEN 'Parc de stationnement ouvert à la circulation publique'
+        WHEN catr = 7 THEN 'Routes de métropole urbaine'
+        WHEN catr = 9 THEN 'autre'
+        ELSE ' '
+    END as categorie_route,
+voie,
+ v1,
+COALESCE(v2,'') as v2,
+    CASE 
+        WHEN circ = 1 THEN 'Non renseigné'
+        WHEN circ = 2 THEN 'A sens unique'
+        WHEN circ = 3 THEN 'Bidirectionnelle'
+        WHEN circ = 4 THEN 'A chaussées séparées'
+        WHEN circ = 5 THEN 'Avec voies d’affectation variable'
+        ELSE ' '
+    END as circulation,
+nbv as nombre_de_voies,
+    CASE
+        WHEN vosp = -1 THEN 'Non renseigné'
+        WHEN vosp = 0 THEN 'Sans objet'
+        WHEN vosp = 1 THEN 'Piste cyclable'
+        WHEN vosp = 2 THEN 'Bande cyclable'
+        WHEN vosp = 3 THEN 'Voie réservée'
+        ELSE ' '
+    END as voie_reservee,
+    CASE
+        WHEN prof = -1 THEN 'Non renseigné'
+        WHEN prof = 1 THEN 'Plat'
+        WHEN prof = 2 THEN 'Pent'
+        WHEN prof = 3 THEN 'Sommet de côte'
+        WHEN prof= 4 THEN 'Bas de côte'
+        ELSE ' '
+    END as profil_route,
+COALESCE(pr,'') as borne,
+pr1 as distance_borne,
+    CASE
+        WHEN plan = -1 THEN 'Non renseigné'
+        WHEN plan = 1 THEN 'Partie rectiligne'
+        WHEN plan = 2 THEN 'En courbe à gauche'
+        WHEN plan = 3 THEN 'En courbe à droite'
+        WHEN plan= 4 THEN 'En « S »'
+        ELSE ' '
+    END as plan,
+COALESCE(lartpc,'') as largeur_tpc,
+COALESCE(larrout, '') as largeur_route,
+    CASE
+        WHEN surf = -1 THEN 'Non renseigné'
+        WHEN surf = 1 THEN 'Normale'
+        WHEN surf = 2 THEN 'Mouillée'
+        WHEN surf = 3 THEN 'Flaques'
+        WHEN surf = 4 THEN 'Inondée'
+        WHEN surf = 5 THEN 'Enneigée'
+        WHEN surf = 6 THEN 'Boue'
+        WHEN surf = 7 THEN 'Verglacée'
+        WHEN surf = 8 THEN 'Corps gras – huile'
+        WHEN surf = 9 THEN 'Autre'
+        ELSE ' '
+    END as surface,
+    CASE
+        WHEN infra = -1 THEN 'Non renseigné'
+        WHEN infra = 0 THEN 'Aucun'
+        WHEN infra = 1 THEN 'Souterrain - tunnel'
+        WHEN infra = 2 THEN 'Pont - autopont'
+        WHEN infra = 3 THEN 'Bretelle d’échangeur ou de raccordement'
+        WHEN infra = 4 THEN 'Voie ferrée'
+        WHEN infra = 5 THEN 'Carrefour aménagé'
+        WHEN infra = 6 THEN 'Zone piétonne'
+        WHEN infra = 7 THEN 'Zone de péage'
+        WHEN infra = 8 THEN 'Chantiers'
+        WHEN infra = 9 THEN 'Autres'
+        ELSE ' '
+    END as infrastructure,
+    CASE
+        WHEN situ = -1 THEN 'Non renseigné'
+        WHEN situ = 0 THEN 'Aucun'
+        WHEN situ = 1 THEN 'Sur chaussée'
+        WHEN situ = 2 THEN 'Sur bande d’arrêt d’urgence'
+        WHEN situ = 3 THEN 'Sur accotement'
+        WHEN situ = 4 THEN 'Sur trottoir'
+        WHEN situ = 5 THEN 'Sur piste cyclable'
+        WHEN situ = 6 THEN 'Sur autre voie spéciale'
+        WHEN situ = 8 THEN 'Autres'
+        ELSE ' '
+    END as situation,
+
+vma as Vit_max_autorisee
+
+FROM velyon-batch-1187.accident.lieux_all
