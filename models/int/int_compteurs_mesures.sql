2dc319f25acb732d1f39baa7208d562c9eaefd73
jurquet@gmail.com <jurquet@gmail.com>
2023-06-07 07:15:50 +0000

    change latitude & longitude to use in compteurs_mesures change join type in channels_sites

--- models/int/int_compteurs_mesures.sql
+++ models/int/int_compteurs_mesures.sql
@@ -5,8 +5,8 @@
 
 SELECT
     mesures.*,
-    sites.lon,
-    sites.lat,
+    sites.latitude,
+    sites.longitude,
     sites.comment
 FROM {{ ref('stg_compteurs_all_mesures') }} AS mesures
-JOIN {{ ref('int_merge_channels_sites') }} AS sites ON mesures.channel_id = sites.channel_id
+JOIN {{ ref('int_merge_channels_sites') }} AS sites ON mesures.channel_id = sites.channel_id AND sites.channel_id IS NOT NULL
