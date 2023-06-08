e8c0066265d038f13d45a9d168742a28ac65d067
Matthias Chanut <saitam.game@gmail.com>
2023-06-08 14:58:46 +0000

    ajustement danger score

---
+++ models/marts/mart_dangerscore_situation.sql
@@ -0,0 +1,20 @@
+WITH sq1 AS (
+    SELECT
+        situation
+        ,CASE 
+            WHEN gravite_blessure = 'Non défini' THEN 1
+            WHEN gravite_blessure = 'Indemne' THEN 2
+            WHEN gravite_blessure = 'Blessé léger' THEN 4
+            WHEN gravite_blessure = 'Blessé hospitalisé' THEN 8
+            WHEN gravite_blessure = 'Non défini' THEN 16
+            END AS danger_score
+    FROM {{ref('int_accidents')}}
+)
+
+SELECT
+    situation
+    ,ROUND(AVG(danger_score),2) AS danger_score
+    ,COUNT(danger_score) as nb_accidents
+FROM sq1
+GROUP BY situation
+ORDER BY danger_score DESC
