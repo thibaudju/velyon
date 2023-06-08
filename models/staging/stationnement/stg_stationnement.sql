f8bb1b8a778d99a00a9f2b4c0ddd03720446ec0f
Clémence BATARDIERE <clemence.batardiere@gmail.com>
2023-06-08 12:15:02 +0000

    amenagement, population, stationnement

--- models/staging/stationnement/stg_stationnement.sql
+++ models/staging/stationnement/stg_stationnement.sql
@@ -6,14 +6,12 @@ SELECT
     int64_field_0 AS id
     ,PARSE_DATE("%Y", CAST(anneerealisation AS STRING)) AS annee
     ,codeinsee AS insee
-    ,* except (int64_field_0, abrite, pole,codeinsee)
+    ,CONCAT(adresse,", ",commune,", ","FRANCE") AS adresse_complete
+    ,* except (int64_field_0, abrite, pole,codeinsee,anneerealisation)
     ,CASE
         WHEN pole IS NULL THEN "non défini"
+        WHEN pole = " " THEN "non défini"
         ELSE pole
         END
         AS pole
 FROM velyon-batch-1187.stationnement.stationnement
-
-
-
-
