5c92e0f1327d0825d40e14fd5f1248e755ab94e7
jurquet@gmail.com <jurquet@gmail.com>
2023-06-09 12:47:19 +0000

    MART compteurs trafic - Correction ajout trafic annuel

--- models/marts/mart_compteurs_index_trafic_annuel.sql
+++ models/marts/mart_compteurs_index_trafic_annuel.sql
@@ -1,6 +1,7 @@
 WITH yearly_norm_trafic AS (
 SELECT 
     EXTRACT (YEAR FROM start_hour) AS year,
+    SUM(nb_passages) AS trafic_annuel,
     AVG(normalized_count) as trafic
 FROM {{ ref('int_compteurs_mesures') }}
 GROUP BY year
@@ -9,7 +10,7 @@ ORDER BY year
 
 SELECT
     year,
-    SUM(trafic) AS trafic,
+    trafic_annuel,
     trafic / MIN (trafic) OVER () AS index_trafic,
     trafic / (LAG (trafic, 1) OVER(ORDER BY year ASC)) -1 AS evo_yoy
 FROM yearly_norm_trafic
