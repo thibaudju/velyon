2fd8df1c2cf89bc9ee87e3b031e68491f7e363a1
Matthias Chanut <saitam.game@gmail.com>
2023-06-12 14:10:17 +0000

    danger score terminé

--- models/marts/mart_danger_score.sql
+++ models/marts/mart_danger_score.sql
@@ -10,41 +10,40 @@ WITH sq1 AS (
             WHEN gravite_blessure = 'Non défini' THEN 1
             WHEN gravite_blessure = 'Indemne' THEN 2
             WHEN gravite_blessure = 'Blessé léger' THEN 4
-            WHEN gravite_blessure = 'Blessé hospitalisé' THEN 8
-            WHEN gravite_blessure = 'Non défini' THEN 16
+            WHEN gravite_blessure = 'Blessé hospitalisé' THEN 6
+            WHEN gravite_blessure = 'Tué' THEN 10
             END AS danger_score
         ,t2.nb_habitants AS population
+        ,t2.geometry
 FROM {{ref('int_accidents')}} t1
-JOIN velyon-batch-1187.dbt_cbatardiere.stg_population t2
-ON t1.commune_insee = t2.insee 
+JOIN {{ref('stg_population')}} t2
+ON CAST(t1.commune_insee AS STRING) = CAST(t2.insee AS STRING)
 ),
 
 --Aggrégation des données
 sq2 AS (
     SELECT
         commune
+        ,geometry
         ,COUNT(Num_Acc) AS nb_accidents
         ,ROUND(AVG(population),0) AS population
         ,ROUND(AVG(danger_score),2) AS danger_score
     FROM sq1
-    GROUP BY commune
+    GROUP BY commune, geometry
 ),
 
 sq3 AS (
     SELECT
     *
     --ratio calculé en * 100 afin d'illustrer un peu plus les tendances 
-    ,ROUND((nb_accidents/population*100),5) AS ratio_acc
-FROM sq2 t1
+    ,ROUND((nb_accidents/population),3) AS ratio_acc
+FROM sq2
 )
 
 --Join avec la table population pour obtenir les données géométriques de chaque commune dans le but d'une viz
 SELECT
     sq3.*
-    ,ROUND((danger_score*(1+ratio_acc)),2) as score_velo
-    ,geometry
+    ,ROUND((danger_score*1+ratio_acc),2) as danger_score_global
 FROM sq3
-LEFT JOIN velyon-batch-1187.dbt_cbatardiere.stg_population t2
-ON sq3.commune = t2.commune
-ORDER BY score_velo DESC
+ORDER BY danger_score_global DESC
 
